cc269709daf8cd15853d40dbcd78366209bf4454
supertorio <tori.kirk@gmail.com>
2019-05-10 15:26:16 +0200

    Setup App framework with transpilers and linting

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import MeisterForm from './components/container/MeisterForm';
+
+// Bootstrap Application
+render(<MeisterForm />, document.getElementById('root'));
